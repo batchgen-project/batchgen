08a92fb8e9a5a0faa37a72051309ed27c33b1967
Andrewxu313 <tairanxu313@gmail.com>
2025-08-06 17:27:51 +0000

    debug

--- moe_gen/models/deepseek/deepseekv3/modeling_deepseek_v3.py
+++ moe_gen/models/deepseek/deepseekv3/modeling_deepseek_v3.py
@@ -1373,7 +1373,7 @@ class DeepseekV3MoE_Decoding_FP8(nn.Module):
 		logits = logits.squeeze(1)  
 		topk_weight, topk_idx = moe_fused_gate(
 			logits, torch.zeros(self.config.n_routed_experts, device=self.device, dtype=torch.float32),
-			self.config.n_routed_experts,
+			self.config.n_group,
 			self.config.topk_group,
 			self.config.num_experts_per_tok,
 			0,
