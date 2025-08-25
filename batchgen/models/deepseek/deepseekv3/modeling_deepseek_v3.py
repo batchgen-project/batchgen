ec82ad59dd268d358eced50ca4693b02149b3b04
Andrewxu313 <tairanxu313@gmail.com>
2025-08-25 16:42:13 +0000

    debug

--- batchgen/models/deepseek/deepseekv3/modeling_deepseek_v3.py
+++ batchgen/models/deepseek/deepseekv3/modeling_deepseek_v3.py
@@ -1437,8 +1437,9 @@ class DeepseekV3MoE_Decoding_FP8(nn.Module):
 		# )
 		if(len(eids) == 0):
 			logger.warning_once("No tokens routed to this rank.")
+			assert len(x) == 0, "If no tokens routed, x should be empty too."
 			return x
-			
+
 		group_size, activated_group_idx, group_start_indices = expert_bincount(
 			eids, self.routed_expert_start_idx, self.experts_per_rank, self.device
 		)
