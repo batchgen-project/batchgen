6b2fb6d9922867ba432af287b6349cf4a5ef9550
Tairan Xu <34982739+Andrewxu313@users.noreply.github.com>
2025-08-28 18:55:47 +0100

    fix: fix OOM when switching from decoding phase to prefill phase (#44)

---
+++ test/r1_mmlu_pro_test/mmlupro_dual_node.sh
@@ -0,0 +1,17 @@
+#!bin/bash
+datetime=$(date '+%Y-%m-%d-%H-%M-%S')
+export HF_ENDPOINT=https://hf-mirror.com
+CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 \
+python <dir-to-r1_mmlu_pro_test.py> \
+    --hugging_face_checkpoint "deepseek-ai/DeepSeek-R1" \
+	--host_kv_cache_size 256 \
+    --max_input_length 4096 \
+    --max_decoding_length 2048 \
+    --ATTN_MODE 3 \
+    --cache_dir <dir to model checkpoint> \
+    --server_host "localhost" \
+    --server_port 9090 \
+	--dist_init_addr <dist-init-addr> \
+	--nnodes 2 \
+	--node_rank <node-rank> \
+    > ./deepseek-r1-bench/${datetime}.log 2>&1
