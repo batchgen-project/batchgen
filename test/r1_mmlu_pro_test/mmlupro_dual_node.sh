408c670b9737c154b85a220becf34a10f973e86e
Tairan Xu <34982739+Andrewxu313@users.noreply.github.com>
2025-08-28 22:02:38 +0100

    fix: change parameter server start logic (#47)

--- test/r1_mmlu_pro_test/mmlupro_dual_node.sh
+++ test/r1_mmlu_pro_test/mmlupro_dual_node.sh
@@ -2,6 +2,16 @@
 datetime=$(date '+%Y-%m-%d-%H-%M-%S')
 export HF_ENDPOINT=https://hf-mirror.com
 CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 \
+<<<<<<< HEAD
+python /data2/tairan/workspace/BatchGen/test/r1_mmlu_pro_test/r1_mmlu_pro_test.py \
+    --hugging_face_checkpoint "deepseek-ai/DeepSeek-R1" \
+	--host_kv_cache_size 256 \
+    --max_prompts 768 \
+    --max_input_length 4096 \
+    --max_decoding_length 4096 \
+    --ATTN_MODE 3 \
+    --cache_dir "/data2/tairan/modelscope/hub/models/deepseek-ai/DeepSeek-R1" \
+=======
 python <dir-to-r1_mmlu_pro_test.py> \
     --hugging_face_checkpoint "deepseek-ai/DeepSeek-R1" \
 	--host_kv_cache_size 256 \
@@ -9,6 +19,7 @@ python <dir-to-r1_mmlu_pro_test.py> \
     --max_decoding_length 2048 \
     --ATTN_MODE 3 \
     --cache_dir <dir to model checkpoint> \
+>>>>>>> 46949f3339712055f2986fbddcf25deb0a2adb93
     --server_host "localhost" \
     --server_port 9090 \
 	--dist_init_addr <dist-init-addr> \
