d18da1796dbb95c021240f2459895a0ccdad8e7e
Andrewxu313 <tairanxu313@gmail.com>
2025-09-08 09:30:52 +0000

    debug

--- test/r1_mmlu_pro_test/r1_mmlu_pro_test.py
+++ test/r1_mmlu_pro_test/r1_mmlu_pro_test.py
@@ -139,7 +139,8 @@ if __name__ == "__main__":
 	if print_result:
 		for idx in range(len(answer_set)):
 		# for idx in range(4):
-			tmp_answer = decode_to_eos(tokenizer, answer_set[idx].tolist())
+			# tmp_answer = decode_to_eos(tokenizer, answer_set[idx].tolist())
+			tmp_answer = tokenizer.decode(answer_set[idx].tolist(), skip_special_tokens=False)
 			# print(f"Prompt {idx}: {queries[idx][:args.max_input_length]}")
 			print("==================================================================")
 			print(f"Query {idx}: {queries[idx][:args.max_input_length]}")
