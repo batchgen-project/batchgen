17d2b50bf7b5a7412ec686a7afa9c1bc70af9062
Andrewxu313 <tairanxu313@gmail.com>
2025-09-16 19:28:56 +0000

    debug

--- batchgen/entrypoint.py
+++ batchgen/entrypoint.py
@@ -17,6 +17,7 @@ from .config.config import EngineConfig
 
 from batchgen.parameter_server_client import ParameterServerClient
 from .utils import config_torch_module_initializer
+from batchgen.engine import BatchGen
 
 logging.basicConfig(
 	level=logging.INFO,  # Set to the lowest level to capture all messages
@@ -297,9 +298,6 @@ class BatchGen():
 			start_query_idx, end_query_idx = distribution[global_rank]
 			logging.info(f"Global rank {global_rank}: Processing queries from {start_query_idx} to {end_query_idx}")
 					
-			# Create batchgen instance with the shared memory info
-			# from batchgen.engine import batchgen
-			from batchgen.engine import BatchGen
 			batchgen_instance = BatchGen(
 				huggingface_ckpt_name=self.huggingface_ckpt_name,
 				hf_cache_dir=self.hf_cache_dir,
