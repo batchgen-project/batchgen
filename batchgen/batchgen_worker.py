dae05ff706318d5baecd0c59e2583fae36f6dc3a
Andrewxu313 <tairanxu313@gmail.com>
2025-09-24 16:39:24 +0000

    debug

--- batchgen/batchgen_worker.py
+++ batchgen/batchgen_worker.py
@@ -78,6 +78,7 @@ class InputArguments:
 	dist_init_addr: Optional[str] = None
 	local_rank: int = 0
 	rank: int = 0
+	global_rank: int = 0
 	world_size: int = 1
 
 	def get(self, key, default=None):
@@ -137,6 +138,7 @@ class BatchGenWorker:
 			"dist_init_addr": dist_init_addr,
 			"local_rank": local_rank,
 			"rank": global_rank,
+			"global_rank": global_rank,
 			"world_size": world_size
 		}
 		# logging.info(f"kv_dtype: {input_arguments['kv_dtype']}")
