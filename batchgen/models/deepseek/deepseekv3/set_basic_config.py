9530a51b5810a1ce504b211d0d9014141ba84506
Andrewxu313 <tairanxu313@gmail.com>
2025-09-24 14:18:58 +0000

    debug

--- batchgen/models/deepseek/deepseekv3/set_basic_config.py
+++ batchgen/models/deepseek/deepseekv3/set_basic_config.py
@@ -2,7 +2,7 @@ from batchgen.config.config import EngineConfig
 import torch
 import logging
 
-def set_basic_config(engine_config: EngineConfig, **kwargs):
+def set_basic_config(engine_config: EngineConfig, input_arguments):
 	"""
 		Basic Config
 	"""
@@ -14,30 +14,30 @@ def set_basic_config(engine_config: EngineConfig, **kwargs):
 	
 	""" KV Dtype """
 	# If kv_dtype is not provided, use bf16
-	if not kwargs.get('kv_dtype', None):
+	if not input_arguments.get('kv_dtype', None):
 		logging.info("kv_dtype is not provided, using bfloat16 as default")
 		engine_config.Basic_Config.kv_dtype = "bfloat16"
 	else:
-		if kwargs.kv_dtype.lower() in ['bfloat16', 'bf16']:
+		if input_arguments.kv_dtype.lower() in ['bfloat16', 'bf16']:
 			engine_config.Basic_Config.kv_dtype = "bfloat16"
-		elif kwargs.kv_dtype.lower() in ['fp8', 'float8', 'float8_e4m3fn']:
+		elif input_arguments.kv_dtype.lower() in ['fp8', 'float8', 'float8_e4m3fn']:
 			engine_config.Basic_Config.kv_dtype = "float8_e4m3fn"
 		else:
-			raise ValueError(f"Unsupported kv_dtype: {kwargs.kv_dtype}, only support ['bfloat16','float8_e4m3fn']")
+			raise ValueError(f"Unsupported kv_dtype: {input_arguments.kv_dtype}, only support ['bfloat16','float8_e4m3fn']")
 	engine_config.Basic_Config.kv_dtype_torch = torch.dtype(engine_config.Basic_Config.kv_dtype)
 
 	""" Attention Dtype """
 	# If attention_dtype is not provided, use bf16
-	if not kwargs.get('attention_dtype', None):
+	if not input_arguments.get('attention_dtype', None):
 		logging.info("attention_dtype is not provided, using bfloat16 as default")
 		engine_config.Basic_Config.attention_dtype = "bfloat16"
 	else:
-		if kwargs.attention_dtype.lower() in ['bfloat16', 'bf16']:
+		if input_arguments.attention_dtype.lower() in ['bfloat16', 'bf16']:
 			engine_config.Basic_Config.attention_dtype = "bfloat16"
-		elif kwargs.attention_dtype.lower() in ['fp8', 'float8', 'float8_e4m3fn']:
+		elif input_arguments.attention_dtype.lower() in ['fp8', 'float8', 'float8_e4m3fn']:
 			engine_config.Basic_Config.attention_dtype = "float8_e4m3fn"
 		else:
-			raise ValueError(f"Unsupported attention_dtype: {kwargs.attention_dtype}, only support ['bfloat16','float8_e4m3fn']")
+			raise ValueError(f"Unsupported attention_dtype: {input_arguments.attention_dtype}, only support ['bfloat16','float8_e4m3fn']")
 	
 
 	""" Activation Dtype """
@@ -45,19 +45,19 @@ def set_basic_config(engine_config: EngineConfig, **kwargs):
 	engine_config.Basic_Config.activation_dtype_torch = torch.dtype(engine_config.Basic_Config.activation_dtype)
 
 	""" Device """
-	if not kwargs.get('device', None):
+	if not input_arguments.get('device', None):
 		raise ValueError("Device must be specified")
 	else:
-		engine_config.Basic_Config.device = kwargs.device
-		engine_config.Basic_Config.device_torch = torch.device(f"cuda:{kwargs.device}")
+		engine_config.Basic_Config.device = input_arguments.device
+		engine_config.Basic_Config.device_torch = torch.device(f"cuda:{input_arguments.device}")
 
 	# """ Attn Mode """
-	# if not kwargs.get('attn_mode', None):
+	# if not input_arguments.get('attn_mode', None):
 	# 	# raise ValueError("Attn mode must be specified")
 	# else:
-	# 	if kwargs.attn_mode not in [1, 2, 3]:
+	# 	if input_arguments.attn_mode not in [1, 2, 3]:
 	# 		raise ValueError("Currently attn_mode must be 1, 2, or 3")
-	# 	engine_config.Basic_Config.attn_mode = kwargs.attn_mode
+	# 	engine_config.Basic_Config.attn_mode = input_arguments.attn_mode
 
 	""" Module Types """
 	engine_config.Basic_Config.module_types = ["attn", "routed_expert", "shared_expert"]
@@ -68,43 +68,43 @@ def set_basic_config(engine_config: EngineConfig, **kwargs):
 	engine_config.Basic_Config.num_threads = 0
 	
 	""" Padding Length """
-	if not kwargs.get('padding_length', None):
+	if not input_arguments.get('padding_length', None):
 		raise ValueError("Padding length must be specified")
 	else:
-		engine_config.Basic_Config.padding_length = kwargs.padding_length
+		engine_config.Basic_Config.padding_length = input_arguments.padding_length
 
 	""" Max Decoding Length """
-	if not kwargs.get('max_decoding_length', None):
+	if not input_arguments.get('max_decoding_length', None):
 		raise ValueError("Max decoding length must be specified")
 	else:
-		engine_config.Basic_Config.max_decoding_length = kwargs.max_decoding_length
+		engine_config.Basic_Config.max_decoding_length = input_arguments.max_decoding_length
 
 
 	""" Num Queries """
-	if not kwargs.get('num_queries', None):
+	if not input_arguments.get('num_queries', None):
 		raise ValueError("Num queries must be specified")
 	else:
-		engine_config.Basic_Config.num_queries = kwargs.num_queries
+		engine_config.Basic_Config.num_queries = input_arguments.num_queries
 
 	""" Rank """
-	if not kwargs.get('rank', None):
+	if not input_arguments.get('rank', None):
 		raise ValueError("Rank must be specified")
 	else:
-		engine_config.Basic_Config.rank = kwargs.rank
+		engine_config.Basic_Config.rank = input_arguments.rank
 
 	""" World Size """
-	if not kwargs.get('world_size', None):
+	if not input_arguments.get('world_size', None):
 		raise ValueError("World size must be specified")
 	else:
-		engine_config.Basic_Config.world_size = kwargs.world_size
+		engine_config.Basic_Config.world_size = input_arguments.world_size
 
 	""" GPU Arch """
-	if not kwargs.get('gpu_arch', None):
+	if not input_arguments.get('gpu_arch', None):
 		raise ValueError("GPU architecture must be specified")
 	else:
-		if kwargs.gpu_arch.lower() not in ['hooper', 'ampere']:
+		if input_arguments.gpu_arch.lower() not in ['hooper', 'ampere']:
 			raise ValueError("Currently gpu_arch must be 'hooper', or 'ampere'")
-		engine_config.Basic_Config.gpu_arch = kwargs.gpu_arch.lower()
+		engine_config.Basic_Config.gpu_arch = input_arguments.gpu_arch.lower()
 
 
 	return engine_config
