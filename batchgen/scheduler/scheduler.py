269b63022d4915461861f7662b92863dab76169e
Andrewxu313 <tairanxu313@gmail.com>
2025-09-24 20:45:20 +0000

    debug

--- batchgen/scheduler/scheduler.py
+++ batchgen/scheduler/scheduler.py
@@ -16,6 +16,7 @@ class Scheduler:
 
 	def generate_config(self) -> EngineConfig:
 		self._set_default_configs()
+		kv_element_size = 2 if self.config.Basic_Config.kv_dtype == "bfloat16" else 1
 		"""
 			Configure the rest.
 		"""
@@ -34,7 +35,7 @@ class Scheduler:
 		est_kv_cp_t_per_micro_batch = attn_decoding_micro_batch_size * self.Max_Context_Length * 576 / (1024 ** 3) / 52 * 1000 # in ms
 		# num_k_buffer = self.compute_profiler.profile(MoE_module) // est_kv_cp_t_per_micro_batch + 2
 		num_k_buffer = 6
-		k_buffer_size = num_k_buffer * attn_decoding_micro_batch_size * self.Max_Context_Length * 576 / (1024 ** 3) # in GB
+		k_buffer_size = num_k_buffer * attn_decoding_micro_batch_size * self.Max_Context_Length * 576 / (1024 ** 3) * kv_element_size # in GB
 
 
 		available_gpu_mem = 96 * DEFAULT_MEM_FRAC  # Assuming 96GB GPU memory
@@ -46,11 +47,12 @@ class Scheduler:
 		available_memory_for_expert_cache = available_gpu_mem - non_static_memory_usage
 		num_local_expert_per_layer = min(EXPERT_PER_RANK, int(available_memory_for_expert_cache // 2.4)) # Each expert cache is around 2.4GB
 		num_decoding_module_buffer_routed_expert = EXPERT_PER_RANK - num_local_expert_per_layer + 2
+
 		if self.config.Basic_Config.attn_mode == 3:
 			num_local_expert_per_layer = EXPERT_PER_RANK
 			expert_size = num_local_expert_per_layer * 2.4
 			# Fix 
-			self.per_seq_size = self.Max_Context_Length * 61 * 576 / (1024 ** 3) # in GB
+			self.per_seq_size = self.Max_Context_Length * 61 * 576 / (1024 ** 3) * kv_element_size # in GB
 			self.config.Module_Batching_Config.MoE_decoding_micro_batch_size = (
 				int((available_gpu_mem - model_skeleton_size - cuda_page_table_default_size - expert_size - NCCL_default_buffer_usage) / self.per_seq_size)
 			)
