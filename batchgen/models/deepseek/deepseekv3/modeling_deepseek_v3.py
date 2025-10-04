478d7c9b9aeef5ce14b2dc6e65d0e6cefe02478a
Andrewxu313 <tairanxu313@gmail.com>
2025-10-04 17:35:58 +0000

    debug

--- batchgen/models/deepseek/deepseekv3/modeling_deepseek_v3.py
+++ batchgen/models/deepseek/deepseekv3/modeling_deepseek_v3.py
@@ -766,7 +766,7 @@ class MoEGate(nn.Module):
 		scores = torch.sigmoid(logits)
 		
 		# Custom kernel handles MoE routing
-		topk_idx, topk_weight = moe_fused_gate(
+		topk_weight, topk_idx = moe_fused_gate(
 			scores,
 			self.e_score_correction_bias,
 			self.n_group,
