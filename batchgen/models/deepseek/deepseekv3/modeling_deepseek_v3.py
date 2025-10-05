9d4a49faf9d2ecd0f49a15dc677125fb1d724445
Andrewxu313 <tairanxu313@gmail.com>
2025-10-05 22:25:59 +0000

    debug

--- batchgen/models/deepseek/deepseekv3/modeling_deepseek_v3.py
+++ batchgen/models/deepseek/deepseekv3/modeling_deepseek_v3.py
@@ -1094,7 +1094,6 @@ from ....moe.fused_grouped_dequant_gemm import (
 from ....moe.fused_dequant_moe import (
 	fused_dequant_weighted_moe_stage_1, 
 	fused_fp8_moe_stage_1,
-	fused_fp8_moe_stage_1_optimized,
 	fused_fp8_moe_stage_1_v2,
 )
 from ....attention.mla.fa3_backend import act_quant
@@ -1758,7 +1757,7 @@ class DeepseekV3MoE_Decoding_FP8(nn.Module):
 
 		# Quantize the recv_x tensor to fp8_e4m3
 		x, x_scale = act_quant(x)
-		intermediate = fused_fp8_moe_stage_1_optimized(
+		intermediate = fused_fp8_moe_stage_1(
 			x, x_scale, 
 			self.gate_list, self.gate_ptrs_ptr,
 			self.up_list, self.up_ptrs_ptr,
