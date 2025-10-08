b3a08f24c356032fcd0521096fd3897c1380d7b2
Andrewxu313 <tairanxu313@gmail.com>
2025-10-08 20:28:27 +0000

    chore: update mmlupro test script

--- test/r1_mmlu_pro_test/mmlupro_dual_node.sh
+++ test/r1_mmlu_pro_test/mmlupro_dual_node.sh
@@ -1,12 +1,17 @@
 #!bin/bash
+# This script is for running the r1_mmlu_pro_test.py on dual nodes with specific configurations.
+# It tests the first 1024 samples of the MMLU Pro test set using the DeepSeek-R1 model.
+# Input prompt length is truncated to 2048 tokens, and the model generates up to 8192 tokens.
+# The script is configured to use fp8 for key-value cache storage and runs on 2 nodes
 datetime=$(date '+%Y-%m-%d-%H-%M-%S')
 export HF_ENDPOINT=https://hf-mirror.com
 CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 \
 python <dir-to-r1_mmlu_pro_test.py> \
     --hugging_face_checkpoint "deepseek-ai/DeepSeek-R1" \
+    --max_prompts 1024 \
 	--host_kv_cache_size 256 \
     --max_input_length 2048 \
-    --max_decoding_length 4096 \
+    --max_decoding_length 8192 \
     --ATTN_MODE 3 \
     --cache_dir <dir to model checkpoint> \
     --server_host "localhost" \
