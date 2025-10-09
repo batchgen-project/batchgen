f07cce9634e4d0024121fa4d22881bd654003dbc
Andrewxu313 <tairanxu313@gmail.com>
2025-10-09 07:34:49 +0000

    debug

--- batchgen/entrypoint.py
+++ batchgen/entrypoint.py
@@ -297,14 +297,15 @@ class BatchGen():
 		for local_rank, global_rank in indices:
 			# start_query_idx = device_idx * queries_per_device
 			# end_query_idx = min((device_idx + 1) * queries_per_device, num_queries)
-			start_query_idx, end_query_idx = distribution[global_rank]
-			logging.info(f"Global rank {global_rank}: Processing queries from {start_query_idx} to {end_query_idx}")
+			# start_query_idx, end_query_idx = distribution[global_rank]
+			# logging.info(f"Global rank {global_rank}: Processing queries from {start_query_idx} to {end_query_idx}")
 					
 			batchgen_instance = BatchGenWorker(
 				huggingface_ckpt_name=self.huggingface_ckpt_name,
 				hf_cache_dir=self.hf_cache_dir,
 				cache_dir=self.cache_dir,
-				queries=self.queries[start_query_idx:end_query_idx],
+				# queries=self.queries[start_query_idx:end_query_idx],
+				queries=self.queries, # Global queries. Leave the packing rules into each worker.
 				max_input_length=self.max_input_length,
 				max_decoding_length=self.max_decoding_length,
 				device=self.device[local_rank],
