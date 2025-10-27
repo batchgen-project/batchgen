abb25a13bc003391e74411977e34ae11587f56b5
Andrewxu313 <tairanxu313@gmail.com>
2025-10-27 00:26:50 +0000

    debug

--- batchgen/attention/mla/fa3_backend.py
+++ batchgen/attention/mla/fa3_backend.py
@@ -431,8 +431,9 @@ def w8a16_gemm(
 	
 	# x_fp8 = per_token_cast_to_fp8(x)
 	x_fp8 = act_quant(x)
-	x_fp8 = (x_fp8[0], get_col_major_tma_aligned_tensor(x_fp8[1]))
-	deep_gemm.gemm_fp8_fp8_bf16_nt(x_fp8, y_fp8, out)
+	# x_fp8 = (x_fp8[0], get_col_major_tma_aligned_tensor(x_fp8[1]))
+	# deep_gemm.gemm_fp8_fp8_bf16_nt(x_fp8, y_fp8, out)
+	deep_gemm.fp8_gemm_nt(x_fp8, y_fp8, out)
 	if activation_bf16.dim() == 3:
 		out = out.view(n_group, l, n)
 	else:
