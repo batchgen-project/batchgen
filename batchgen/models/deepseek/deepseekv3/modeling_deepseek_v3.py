94e45d51a4e31a221c9fd259efc311bba0abc344
Andrewxu313 <tairanxu313@gmail.com>
2025-11-16 15:13:01 +0000

    debug

--- batchgen/models/deepseek/deepseekv3/modeling_deepseek_v3.py
+++ batchgen/models/deepseek/deepseekv3/modeling_deepseek_v3.py
@@ -1567,34 +1567,34 @@ class DeepseekV3MoE_Decoding_FP8(nn.Module):
 		# possible inputs (e.g., max_batch_size * max_seq_len * K)
 		# We'll use getattr to provide a reasonable default.
 		# self.max_moe_buffer_tokens = getattr(config, 'max_moe_buffer_tokens', 1024)
-		self.max_recv_tokens = getattr(config, 'max_moe_buffer_tokens', 1024)
+		# self.max_recv_tokens = getattr(config, 'max_moe_buffer_tokens', 1024)
 		
-		# We need hidden_size and dtype. We assume they are in the config
-		# as they are required for the DeepseekV3MLP module.
-		hidden_size = config.hidden_size
-		dtype = torch.bfloat16
+		# # We need hidden_size and dtype. We assume they are in the config
+		# # as they are required for the DeepseekV3MLP module.
+		# hidden_size = config.hidden_size
+		# dtype = torch.bfloat16
 		
-		# We assume the expert ID dtype is int64, as this is the default
-		# output of .sort() and .bincount().
-		eid_dtype = torch.int64
+		# # We assume the expert ID dtype is int64, as this is the default
+		# # output of .sort() and .bincount().
+		# eid_dtype = torch.int64
 		
-		self.register_buffer(
-			"recv_x_buffer",
-			torch.empty(
-				self.max_recv_tokens,
-				hidden_size,
-				device=self.device,
-				dtype=dtype
-			)
-		)
-		self.register_buffer(
-			"recv_eid_buffer",
-			torch.empty(
-				self.max_recv_tokens,
-				device=self.device,
-				dtype=eid_dtype
-			)
-		)
+		# self.register_buffer(
+		# 	"recv_x_buffer",
+		# 	torch.empty(
+		# 		self.max_recv_tokens,
+		# 		hidden_size,
+		# 		device=self.device,
+		# 		dtype=dtype
+		# 	)
+		# )
+		# self.register_buffer(
+		# 	"recv_eid_buffer",
+		# 	torch.empty(
+		# 		self.max_recv_tokens,
+		# 		device=self.device,
+		# 		dtype=eid_dtype
+		# 	)
+		# )
 
 
 
@@ -1640,32 +1640,32 @@ class DeepseekV3MoE_Decoding_FP8(nn.Module):
 		self.gate_bias = torch.zeros(self.config.n_routed_experts, device=self.device, dtype=torch.bfloat16)
 		
 		# Pre-allocate symmetric memory buffers
-		# max_inp_len = self.num_tokens_per_rank * self.num_experts_per_tok
-		# max_out_len = max_inp_len * dist.get_world_size()
+		max_inp_len = self.num_tokens_per_rank * self.num_experts_per_tok
+		max_out_len = max_inp_len * dist.get_world_size()
 		
-		# self.symm_inp = symm_mem.empty(
-		# 	max_inp_len, self.config.hidden_size, 
-		# 	dtype=torch.bfloat16, device=self.device
-		# )
+		self.symm_inp = symm_mem.empty(
+			max_inp_len, self.config.hidden_size, 
+			dtype=torch.bfloat16, device=self.device
+		)
 		# symm_inp_hdl = symm_mem.rendezvous(self.symm_inp, dist.group.WORLD)
-		# self.symm_out = symm_mem.empty(
-		# 	max_out_len, self.config.hidden_size, 
-		# 	dtype=torch.bfloat16, device=self.device
-		# )
+		self.symm_out = symm_mem.empty(
+			max_out_len, self.config.hidden_size, 
+			dtype=torch.bfloat16, device=self.device
+		)
 		# symm_out_hdl = symm_mem.rendezvous(self.symm_out, dist.group.WORLD)
-		# self.symm_in_splits = symm_mem.empty(
-		# 	self.total_experts, dtype=torch.int64, device=self.device
-		# )
+		self.symm_in_splits = symm_mem.empty(
+			self.total_experts, dtype=torch.int64, device=self.device
+		)
 		# symm_in_splits_hdl = symm_mem.rendezvous(self.symm_in_splits, dist.group.WORLD)
-		# self.symm_out_splits_offsets = symm_mem.empty(
-		# 	(2, self.total_experts), dtype=torch.int64, device=self.device
-		# )
+		self.symm_out_splits_offsets = symm_mem.empty(
+			(2, self.total_experts), dtype=torch.int64, device=self.device
+		)
 		# symm_out_splits_offsets_hdl = symm_mem.rendezvous(self.symm_out_splits_offsets, dist.group.WORLD)
-		# self.symm_in_splits_offsets = symm_mem.empty(
-		# 	(2, self.total_experts), dtype=torch.int64, device=self.device
-		# )
+		self.symm_in_splits_offsets = symm_mem.empty(
+			(2, self.total_experts), dtype=torch.int64, device=self.device
+		)
 		# symm_in_splits_offsets_hdl = symm_mem.rendezvous(self.symm_in_splits_offsets, dist.group.WORLD)
-		# self.group_name = dist.group.WORLD.group_name
+		self.group_name = dist.group.WORLD.group_name
 
 	def init(self, num_tokens_per_rank):
 		# self.num_tokens_per_rank = num_tokens_per_rank
@@ -1712,7 +1712,7 @@ class DeepseekV3MoE_Decoding_FP8(nn.Module):
 		orig_shape = hidden_states.shape
 		hidden_states = hidden_states.view(-1, hidden_states.shape[-1])
 		identity = hidden_states
-		out = self.moe_infer_alltoall_sendrecv_v3(hidden_states)
+		out = self.moe_infer_alltoall_nvshmem(hidden_states)
 		out = out + self.shared_experts(identity)
 		return out.view(*orig_shape)
 	
