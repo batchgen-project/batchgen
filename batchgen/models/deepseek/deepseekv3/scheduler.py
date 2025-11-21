2a336ac93ca62b271b70fc28fc7343f9c327f2a6
s2531999 <t.xu-29@sms.ed.ac.uk>
2025-11-21 20:21:08 +0000

    debug

--- batchgen/models/deepseek/deepseekv3/scheduler.py
+++ batchgen/models/deepseek/deepseekv3/scheduler.py
@@ -24,7 +24,7 @@ class Scheduler:
 		"""
 			Configure the rest.
 		"""
-		DEFAULT_MEM_FRAC = 0.80
+		DEFAULT_MEM_FRAC = 0.82
 		# MAGIC_NUM = self.compute_profiler.profile(attn_decoding_module)
 		MAGIC_NUM = 224000 * 3
 		EXPERT_PER_RANK = 256 // self.world_size
