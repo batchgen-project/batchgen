cac28702939426daa3f2a3f1282992b35cf0ed6f
TairanXU <t.xu-29@sms.ed.ac.uk>
2026-04-28 18:41:34 +0800

    Fix V4 parameter server path type

--- batchgen/models/deepseek/deepseekv4_flash/deepseekv4_flash_parameter_server.py
+++ batchgen/models/deepseek/deepseekv4_flash/deepseekv4_flash_parameter_server.py
@@ -88,7 +88,7 @@ class DeepSeekV4Flash_Parameter_Server:
             self.shm_name,
             self.tensor_meta_shm_name,
             byte_size,
-            self.converted_ckpt_dir,
+            str(self.converted_ckpt_dir),
             self.state_dict_name_map,
         )
         return self.shm_name, self.tensor_meta_shm_name
