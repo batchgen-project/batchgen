537550bbd26d2dc8047dc20814ecab16e80122ef
TairanXU <t.xu-29@sms.ed.ac.uk>
2026-07-09 02:47:35 +0800

    deepseek v2/v3: shim is_torch_fx_available removed in transformers 5.x

--- batchgen/models/deepseek/deepseekv3/modeling_deepseek_v3.py
+++ batchgen/models/deepseek/deepseekv3/modeling_deepseek_v3.py
@@ -53,7 +53,11 @@ from transformers.utils import (
 	logging,
 	replace_return_docstrings,
 )
-from transformers.utils.import_utils import is_torch_fx_available
+try:
+    from transformers.utils.import_utils import is_torch_fx_available
+except ImportError:  # removed in newer transformers; fx tracing unused at serve time
+    def is_torch_fx_available():
+        return False
 from .configuration_deepseek_v3 import DeepseekV3Config
 import torch.distributed as dist
 import numpy as np
